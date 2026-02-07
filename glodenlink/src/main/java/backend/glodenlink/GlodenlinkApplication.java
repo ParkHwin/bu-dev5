ae0774f68a57c22eca5290713340e377e588a53b
jaehwan0706 <jaehwan0706@gmail.com>
2026-02-07 14:44:15 +0900

    backend 환경구성

---
+++ glodenlink/src/main/java/backend/glodenlink/GlodenlinkApplication.java
@@ -0,0 +1,13 @@
+package backend.glodenlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlodenlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlodenlinkApplication.class, args);
+	}
+
+}
